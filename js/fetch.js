e974543bccdfdf2944774db366956752a0bbce4f
Feltorlorongy <megyery.bese@szikszi-ozd.hu>
2025-05-22 13:09:31 +0200

    Első kommit, félkész.

---
+++ js/fetch.js
@@ -0,0 +1,31 @@
+setInterval(() => {
+    fetch('https://meowfacts.herokuapp.com')
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Hálózati válasz nem volt megfelelő.")  
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("").innerText = data[0];
+        })
+        .catch(error => {
+            console.error("Hiba történt a következő művelet során:", error, " :c");
+        })
+
+    
+    fetch('https://api.thecatapi.com/v1/images/search?limit=1')
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Hálózati válasz nem volt megfelelő.")  
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("full").backgroundImage = url(data[0].url);
+        })
+        .catch(error => {
+            console.error("Hiba történt a következő művelet során:", error, " :c");
+        })
+}, 5000)
+
